d4d6478e082e479b914010e0fcafca4b2ff75ff8
Faiz Shaikh <faizshaikh8869@gmail.com>
2026-09-12 10:52:15 +0530

    Update CSS import

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import './index.css'
+import './style.css'
 // import { CollectionsProvider } from './context/CollectionContext.jsx'
 
 import App from './App.jsx'
